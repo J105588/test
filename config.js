6c6d2c3a337fe8d19181ced52abb58b766f9632d
J105588 <jxjin2010@gmail.com>
2025-08-23 01:47:39 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyr_lHjQUUKLP6y8-OwxHTdx5NbVOKl6vpNGUwYBYpDZgNe30isnztEPJN1xzPuKEXn/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxgBX68FOMrQUn9Ix8JDxDimrzaTwe8chlOifcw6aiOQUgRtLY93bTd9xzmFgEWdUic/exec";
 const DEBUG_MODE = true;
 
 function debugLog(message, obj = null) {
